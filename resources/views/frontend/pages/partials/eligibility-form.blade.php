ad276ae428ce6e4f232967457f45f3540303a54f
mannan608 <mannanswe608@gmail.com>
2026-05-22 16:25:14 +0600

    change eligibility UI

--- resources/views/frontend/pages/partials/eligibility-form.blade.php
+++ resources/views/frontend/pages/partials/eligibility-form.blade.php
@@ -1,5 +1,5 @@
 
- <section class="bg-brand-500/10 py-10 sm:py-12 lg:py-16">
+ <section class=" py-10 sm:py-12 lg:py-16">
         <div class="max-w-7xl mx-auto px-5 md:px-8">
             <!-- Heading -->
             <div class="text-center md:mb-8 mb-4">
@@ -8,9 +8,9 @@
                 <p class="text-on-surface-variant max-w-xl mx-auto text-sm md:text-base">Check Eligibility For Recognition of Prior Learning - RPL</p>
             </div>
 
-            <div class="flex gap-10 items-center">
-                <div class="">
-                    <img src="{{ asset('frontend-img/eligibility.jpg') }}" alt="">
+            <div class="flex flex-col md:flex-row gap-2 md:gap-10 items-center">
+                <div class="w-full h-auto rounded">
+                    <img src="{{ asset('frontend-img/eligibility.jpg') }}" class="rounded" alt="check eligibility">
                 </div>
                <iframe src="https://api.leadconnectorhq.com/widget/survey/fiJUZlVsbbtUgA4nzMP1" style="border:none;width:100%;" scrolling="no" id="fiJUZlVsbbtUgA4nzMP1" title="survey"></iframe>
             </div>         
