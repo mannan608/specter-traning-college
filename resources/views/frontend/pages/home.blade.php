8446405962b2337c1f5cd0f62ab778186d5750bd
mannan608 <mannanswe608@gmail.com>
2026-06-11 09:54:55 +0600

    update

--- resources/views/frontend/pages/home.blade.php
+++ resources/views/frontend/pages/home.blade.php
@@ -15,7 +15,7 @@
                 {{-- Heading --}}
                 <h1
                     class="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-slate-900 leading-tight max-w-2xl text-center sm:text-start">
-                    mannan Your Career with
+                    Elevate Your Career with
                     <span class="text-brand-600">
                         Industry-Leading
                     </span>
